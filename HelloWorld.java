91f7d269f65e1d210585a49757e67e609fb6c6df
V-2000 <60283457+V-2000@users.noreply.github.com>
2026-07-03 16:17:19 +0530

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, World");
      System.out.println("Hello, Vikash");
-     System.out.println("Hello, devopsbyViki112"); 
+     System.out.println("Hello, devopsbyViki1122"); 
 
   }
 }
